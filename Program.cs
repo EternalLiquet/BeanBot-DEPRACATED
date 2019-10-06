3e7c4d1ae45c733684a85b8e8cc8125a54222e43
EternalLiquet <aldmnatividad@gmail.com>
2019-10-06 14:12:02 -0400

    add nuget packages and logger

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.WebSocket;
+
+using Serilog;
+using Serilog.Configuration;
 
 namespace BeanBot
 {
     class Program
     {
+        private DiscordSocketClient _client;
+        private string _token;
+
         static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\BeanBotLogs.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
         }
     }
 }
---
+++ Util/Support.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeanBot.Util
+{
+    public static class Support
+    {
+        static string botToken;
+
+        public static void StartupOperations()
+        {
+
+        }
+    }
+}
