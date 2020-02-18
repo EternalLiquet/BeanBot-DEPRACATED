362250b89dc7404ae205323701e47f380550978d
Aldrin Natividad <aldrin.natividad@adesa.com>
2020-02-18 16:43:22 -0500

    Refactor Support

--- BeanBot/Util/Support.cs
+++ BeanBot/Util/Support.cs
@@ -4,17 +4,12 @@ namespace BeanBot.Util
 {
     public static class Support
     {
-        public static string BotToken { get; private set; }
-        public static long ILServerId { get; private set; }
-
         public static void StartupOperations()
         {
             LogHandler.CreateLoggerConfiguration();
             DirectorySetup.MakeSureAllDirectoriesExist();
-            OptionsSetup.MakeSureBeanTokenFileExists();
-            OptionsSetup.MakeSureServerIDFileExists();
-            BotToken = OptionsSetup.GetBeanTokenFromBeanTokenFile();
-            ILServerId = OptionsSetup.GetServerIdFromServerIdFile();
+            AppSettings.MakeSureSettingsJsonExists();
+            AppSettings.ReadSettingsFromFile();
             Log.Information("Startup Operations complete");
         }
     }
