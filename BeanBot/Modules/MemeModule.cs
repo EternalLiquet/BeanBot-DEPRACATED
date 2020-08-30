e3a7228db060ae61a4e94a4cc5def52926aecba7
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-08-30 11:56:39 -0400

    Meme Command

--- BeanBot/Modules/MemeModule.cs
+++ BeanBot/Modules/MemeModule.cs
@@ -14,6 +14,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace BeanBot.Modules
 {
@@ -148,30 +149,28 @@ namespace BeanBot.Modules
         [Summary("Will give you a random meme from reddit")]
         [Remarks("meme")]
         [RequireBotPermission(ChannelPermission.SendMessages)]
-        public async Task Meme()
+        public async Task Meme(string subreddit = "")
         {
-            await Task.Factory.StartNew(() => { _ = InvokeMemeApi(); });
+            await Task.Factory.StartNew(() => { _ = InvokeMemeApi(subreddit); });
         }
-        private async Task InvokeMemeApi()
+        private async Task InvokeMemeApi(string subreddit)
         {
-            try
+            string uri;
+            if (string.IsNullOrEmpty(subreddit)) uri = $"https://meme-api.herokuapp.com/gimme";
+            else uri = $"https://meme-api.herokuapp.com/gimme/{HttpUtility.UrlEncode(subreddit)}";
+            HttpResponseMessage response = await httpClient.GetAsync(requestUri: uri);
+            if (!response.IsSuccessStatusCode) await ReplyAsync("The meme machine is down, quick, call 911!");
+            else
             {
-                HttpResponseMessage response = await httpClient.GetAsync("https://meme-api.herokuapp.com/gimme");
-                if (!response.IsSuccessStatusCode) await ReplyAsync("The meme machine is down, quick, call 911!");
-                else
+                MemeResponse meme = JsonConvert.DeserializeObject<MemeResponse>(await response.Content.ReadAsStringAsync());
+                EmbedBuilder memeBuilder = new EmbedBuilder()
                 {
-                    MemeResponse meme = JsonConvert.DeserializeObject<MemeResponse>(await response.Content.ReadAsStringAsync());
-                    EmbedBuilder memeBuilder = new EmbedBuilder()
-                    {
-                        Title = meme.title,
-                        Description = $"/r/{meme.subreddit}",
-                        ImageUrl = meme.url
-                    };
-                    await ReplyAsync(embed: memeBuilder.Build());
-                }
+                    Title = meme.title,
+                    Description = $"/r/{meme.subreddit}",
+                    ImageUrl = meme.url
+                };
+                await ReplyAsync(embed: memeBuilder.Build());
             }
-            catch (Exception e) { Log.Error(e.Message); }
-
         }
 
 
