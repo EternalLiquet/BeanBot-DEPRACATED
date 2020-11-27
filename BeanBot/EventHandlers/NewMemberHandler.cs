0a284eadd8679da6e37f157f38150a1a557623b0
Aldrin Natividad <aldmnatividad@gmail.com>
2020-11-27 11:55:08 -0500

    Re-enable New Member Handler

--- BeanBot/EventHandlers/NewMemberHandler.cs
+++ BeanBot/EventHandlers/NewMemberHandler.cs
@@ -1,39 +1,39 @@
-﻿//using BeanBot.Util;
-//using Discord.WebSocket;
-//using Serilog;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using BeanBot.Util;
+using Discord.WebSocket;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace BeanBot.EventHandlers
-//{
-//    class NewMemberHandler
-//    {
-//        private readonly DiscordSocketClient _discordClient;
+namespace BeanBot.EventHandlers
+{
+    class NewMemberHandler
+    {
+        private readonly DiscordSocketClient _discordClient;
 
-//        public NewMemberHandler(DiscordSocketClient client)
-//        {
-//            this._discordClient = client;
-//        }
+        public NewMemberHandler(DiscordSocketClient client)
+        {
+            this._discordClient = client;
+        }
 
-//        public void InitializeNewMembers()
-//        {
-//            _ = Task.Factory.StartNew(() => { HandleNewMember(); });
-//        }
+        public void InitializeNewMembers()
+        {
+            _ = Task.Factory.StartNew(() => { HandleNewMember(); });
+        }
 
-//        private void HandleNewMember()
-//        {
-//            Log.Information("Initializing New Member Handler");
-//            _discordClient.UserJoined += async (u) =>
-//            {
-//                if (u.IsBot) return;
-//                var userDMChannel = await u.GetOrCreateDMChannelAsync();
-//                Log.Debug("Successfully created user DM channel");
-//                await userDMChannel.SendMessageAsync("Please read the rules in the Eli's Charter channel. If you agree to these rules and are over the age of 17, please use command %shine");
-//                Log.Debug("Successfully sent message");
-//            };
-//            _discordClient.UserJoined += LogHandler.LogNewMember;
-//        }
-//    }
-//}
+        private void HandleNewMember()
+        {
+            Log.Information("Initializing New Member Handler");
+            _discordClient.UserJoined += async (u) =>
+            {
+                if (u.IsBot) return;
+                var userDMChannel = await u.GetOrCreateDMChannelAsync();
+                Log.Debug("Successfully created user DM channel");
+                await userDMChannel.SendMessageAsync("Please read the rules in the Eli's Charter channel. If you agree to these rules and are over the age of 17, please DM <@95570717537480704>");
+                Log.Debug("Successfully sent message");
+            };
+            _discordClient.UserJoined += LogHandler.LogNewMember;
+        }
+    }
+}
