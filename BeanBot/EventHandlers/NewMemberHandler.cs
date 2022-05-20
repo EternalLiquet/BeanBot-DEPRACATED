31c361302238ad1f4dd8d3169e1c275271b892f1
Aldrin Natividad <aldmnatividad@gmail.com>
2022-05-20 17:56:56 -0400

    Update NewMemberHandler.cs

--- BeanBot/EventHandlers/NewMemberHandler.cs
+++ BeanBot/EventHandlers/NewMemberHandler.cs
@@ -31,7 +31,7 @@ namespace BeanBot.EventHandlers
                 if (u.IsBot) return;
                 var userDMChannel = await u.GetOrCreateDMChannelAsync();
                 Log.Debug("Successfully created user DM channel");
-                await userDMChannel.SendMessageAsync("Please read the rules in the Eli's Charter channel. If you agree to these rules and are over the age of 17, please DM <@203684483826712577>, <@164206456407719937>, or <@221311562848272384>");
+                await userDMChannel.SendMessageAsync("Please read the rules in the Eli's Charter channel. If you agree to these rules and are over the age of 17, please DM one of the moderators (i.e discount Hatate/Makoto Kikuchi#2351) for full access to the server! (I promise it's worth it)");
                 Log.Debug("Successfully sent message");
             };
             _discordClient.UserJoined += LogHandler.LogNewMember;
