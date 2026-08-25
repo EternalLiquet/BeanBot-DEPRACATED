4e30c8dd0f8875fc9ace6a932945440bc1ce6c82
Aldrin Natividad <aldmnatividad@gmail.com>
2026-08-25 10:06:50 -0400

    Add bounded external media command options

---
+++ BeanBot/Discord/Commands/ExternalMediaCommandOptions.cs
@@ -0,0 +1,35 @@
+namespace BeanBot.Discord.Commands;
+
+internal sealed class ExternalMediaCommandOptions
+{
+    internal static ExternalMediaCommandOptions Default { get; } = new(
+        imageDownloadTimeout: TimeSpan.FromSeconds(10),
+        discordUploadTimeout: TimeSpan.FromSeconds(10),
+        memeApiTimeout: TimeSpan.FromSeconds(10),
+        maxImageBytes: 8 * 1024 * 1024);
+
+    internal ExternalMediaCommandOptions(
+        TimeSpan imageDownloadTimeout,
+        TimeSpan discordUploadTimeout,
+        TimeSpan memeApiTimeout,
+        int maxImageBytes)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(imageDownloadTimeout, TimeSpan.Zero);
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(discordUploadTimeout, TimeSpan.Zero);
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(memeApiTimeout, TimeSpan.Zero);
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(maxImageBytes, 0);
+
+        ImageDownloadTimeout = imageDownloadTimeout;
+        DiscordUploadTimeout = discordUploadTimeout;
+        MemeApiTimeout = memeApiTimeout;
+        MaxImageBytes = maxImageBytes;
+    }
+
+    internal TimeSpan ImageDownloadTimeout { get; }
+
+    internal TimeSpan DiscordUploadTimeout { get; }
+
+    internal TimeSpan MemeApiTimeout { get; }
+
+    internal int MaxImageBytes { get; }
+}
