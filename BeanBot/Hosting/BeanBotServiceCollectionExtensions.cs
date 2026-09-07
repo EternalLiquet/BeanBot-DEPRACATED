2f03890a8df7b2ff8833da339dd3af6cb1e02d7f
Aldrin Natividad <aldmnatividad@gmail.com>
2026-09-07 10:12:20 -0400

    Register daily pun claim store

--- BeanBot/Hosting/BeanBotServiceCollectionExtensions.cs
+++ BeanBot/Hosting/BeanBotServiceCollectionExtensions.cs
@@ -52,6 +52,7 @@ internal static class BeanBotServiceCollectionExtensions
             new MongoClient(provider.GetRequiredService<BeanBotOptions>().MongoConnectionString));
         services.AddSingleton<IMongoDatabase>(provider =>
             provider.GetRequiredService<MongoClient>().GetDatabase("BeanBotDB"));
+        services.AddSingleton<IDailyPunClaimStore, MongoDailyPunClaimStore>();
 
         services.AddSingleton<DiscordConnectionHealth>();
         services.AddSingleton<DiscordLifecycleCoordinator>();
@@ -121,7 +122,7 @@ internal static class BeanBotServiceCollectionExtensions
         services.AddSingleton<PunProvider>();
         services.AddSingleton<IPunProvider>(provider =>
             provider.GetRequiredService<PunProvider>());
-        services.AddSingleton(ExternalMediaCommandOptions.Default);
+        services.AddSingleton<ExternalMediaCommandOptions.Default);
         services.AddSingleton<ExternalImageClient>();
         services.AddSingleton<IExternalImageClient>(provider =>
             provider.GetRequiredService<ExternalImageClient>());
