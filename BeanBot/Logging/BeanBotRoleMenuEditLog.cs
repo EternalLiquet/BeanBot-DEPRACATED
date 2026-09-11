1c4da776204497d1a99fc091b1557ac6f07c39e9
Aldrin Natividad <aldmnatividad@gmail.com>
2026-09-11 10:14:24 -0400

    Add role menu edit failure logs

---
+++ BeanBot/Logging/BeanBotRoleMenuEditLog.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Logging;
+
+namespace BeanBot.Logging;
+
+internal static partial class BeanBotLog
+{
+    [LoggerMessage(
+        Level = LogLevel.Error,
+        Message = "Role-menu edit persistence outcome was not confirmed for menu {MenuId}")]
+    internal static partial void RoleMenuEditPersistenceFailed(
+        ILogger logger,
+        string menuId,
+        Exception exception);
+
+    [LoggerMessage(
+        Level = LogLevel.Error,
+        Message = "Role-menu edit panel update outcome was not confirmed for menu {MenuId}")]
+    internal static partial void RoleMenuEditPanelUpdateFailed(
+        ILogger logger,
+        string menuId,
+        Exception exception);
+}
